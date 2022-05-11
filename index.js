e80c9a3fb7189909ab147666dfedffc63021d1bc
Ardula Anjaneyulu <Ardula.Anjaneyulu@novigo.com>
2022-05-11 22:21:51 +0530

    Added:register and login api added

--- index.js
+++ index.js
@@ -2,14 +2,74 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const DB = require('./db');
+const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt")
 const { query } = require('express');
+const auth = require('./middleware/auth')
+
 
 
 //middileware 
 app.use(cors());
 app.use(express.json());
 
-app.post('/post', async (req, res, next) => {
+app.post('/register', async (req, res, next) => {
+  const db = await DB();
+  const { email, name, password } = req.body
+  try {
+    var query = `SELECT * FROM users WHERE email='${email}'`;
+    var result = await db.query(query);
+    console.log("all details", result);
+    if (result.rowCount> 0) {
+      return res.status(400).json("user already exist")
+    } else {
+      const salt = await bcrypt.genSalt(8);
+      const bycrpass = await bcrypt.hash(password, salt)
+      var query = `INSERT INTO users(email, name, password) VALUES ('${email}','${name}','${bycrpass}' )RETURNING *`
+      let newUser = await db.query(query)
+      newUser.message = "user created succesfully";
+      res.status(200).json(newUser.rows[0])
+    }
+
+
+  } catch (err) {
+    next(err)
+    console.log("error while register", err);
+  }
+})
+
+app.post('/login', async (req,res, next)=>{
+  const db = await DB();
+  const {email, password}=req.body;
+  try{
+    var query = `SELECT * FROM users WHERE email='${email}'`;
+    var result = await db.query(query)
+    console.log("all details", result);
+    if(result.rows[0]==0){
+      return res.status(401).json(result)
+    }else{
+      const hashpass = result.rows[0].password;
+      const validPass = await bcrypt.compare(password, hashpass)
+     
+      if(validPass){
+        return res.status(401).json("login succs")
+      }else{
+        return res.status(401).json("failed")
+      }
+    }
+   
+  
+  }catch(err){
+    next(err)
+    console.log("error while login", err);
+    
+
+  }
+})
+
+
+
+app.post('/post', auth, async (req, res, next) => {
   console.log("calling api");
   const db = await DB();
   try {
---
+++ middleware/auth.js
@@ -0,0 +1,73 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+//middleware will continue if the token is inside the local storage
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header("authorization");
+
+  // return if there is no token
+  if (!token) {
+    return res.status(403).json({ msg: "authorization denied" });
+  }
+
+  // Verify token
+  try {
+    //it is going to give the user id (user:{id: user.id})
+    const verify = jwt.verify(token, process.env.jwtSecret);
+
+    req.user = verify.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+}
+
+// const jwt = require("jsonwebtoken");
+// const jwksRsa  = require('jwks-rsa');
+// const dotenv = require('dotenv');
+
+// // get config vars
+// dotenv.config();
+
+// //configuring the jwt client with the identity server jwt url
+// var client = jwksRsa({
+//     jwksUri: process.env.JWT_URL
+   
+//   });
+//  // console.log(client);
+
+//   async function getKey(header){
+//     try {
+//      // console.log("client.getSigningKey(header.kid)", header.kid)
+//       var key = await client.getSigningKey(header.kid);
+//       //console.log("key :", key);
+//       var signingKey = key.publicKey || key.rsaPublicKey;
+//       //console.log("signing key :", signingKey);
+//       return signingKey;
+//     } catch (e) {
+//       console.log(e)
+//     }
+//   }
+//   //validating the user jwt token
+// const verifyToken = async (req, res, next) => {
+//     var token = null;
+//     // console.log(req.headers)
+//     if(req.headers["authorization"]) token =  req.headers["authorization"].split(' ')[1];
+//     //console.log(token);
+//     if (!token) {
+//       return next({status:403, message:"Authentication is required."});
+//     }
+//     try {
+//       var scecretKey = await getKey(req.headers);
+//       //console.log("secret key :", scecretKey);
+//       const decoded = jwt.verify(token, scecretKey);
+//       req.user = decoded;
+//       //console.log("decoded user data:", req.user);
+//       return next();
+//     } catch (err) {
+//       return next(err);
+//     }
+//   };
+
+//   module.exports = verifyToken;
